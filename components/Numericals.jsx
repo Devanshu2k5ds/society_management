982491c5f2e52bf7c2b0996c073f5e70fcd37d71
Devanshu singla <singladevanshu925@gmail.com>
2025-10-10 12:08:50 +0530

    added the card components

--- components/Numericals.jsx
+++ components/Numericals.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Numericals = () => {
   return (
-    <div>
-    
+    <div className='flex justify-around items-center h-30'>
+     <div>
+        <h1 className='text-3xl font-medium text-blue-600'>25+</h1>
+        <p>Active Societies</p>
+     </div>
+     <div>
+        <h1 className='text-3xl font-medium text-green-600'>150+</h1>
+        <p>Monthly Events</p>
+     </div>
+     <div>
+        <h1 className='text-3xl font-medium text-purple-600'>2.5k+</h1>
+        <p>Active Members</p>
+     </div>
+     <div>
+        <h1 className='text-3xl font-medium text-red-600'>98%</h1>
+        <p>Satisfaction Rate</p>
+     </div>
     </div>
   )
 }
--- src/pages/first.jsx
+++ src/pages/first.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { useAppContext } from '../../context/AppContext';
 import Mainbanner from '../../components/Mainbanner';
 import Numericals from '../../components/numericals';
-const First = () =>{
-    const {count,setCount } = useAppContext();
+import Society_card from '../../components/Society_card';
+import Tech_Image from '../../src/assets/tech.jpeg'
+
+const First = () => {
+    const { count, setCount } = useAppContext();
     return (
         <div>
-        <Mainbanner/>
-        <Numericals/>
+            <Mainbanner />
+            <Numericals />
+            <div className='space-y-5 my-5 mx-auto w-[600px]'>
+            <h1 className='text-4xl font-medium ' >Our Partner Socities</h1>
+            <p className='text-gray-600 mb-7 '>Discover the diverse range of socities that make our college community vibrant and engaging</p>
+            </div>
+            <div className="mx-auto max-w-7xl py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-fr">
+                <Society_card src={Tech_Image} alt="Tech_Image" title="Tech Societies" Content="Leading technology innovation and coding competitions across campus." joiny={450} />
+                <Society_card src={Tech_Image} alt="Tech_Image" title="Tech Societies" Content="Leading technology innovation and coding competitions across campus." joiny={450} />
+                <Society_card src={Tech_Image} alt="Tech_Image" title="Tech Societies" Content="Leading technology innovation and coding competitions across campus." joiny={450} />
+                <Society_card src={Tech_Image} alt="Tech_Image" title="Tech Societies" Content="Leading technology innovation and coding competitions across campus." joiny={450} />
+                <Society_card src={Tech_Image} alt="Tech_Image" title="Tech Societies" Content="Leading technology innovation and coding competitions across campus." joiny={450} />
+                <Society_card src={Tech_Image} alt="Tech_Image" title="Tech Societies" Content="Leading technology innovation and coding competitions across campus." joiny={450} />
+            </div>
         </div>
-)
+    )
 };
-export default First;   
+export default First;
